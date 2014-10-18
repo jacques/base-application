7563988711b9159a8ac5916e1389e14b5efe5fa7
Jacques Marneweck <jacques@powertrip.co.za>
2014-10-18 09:23:53 +0000

    Use bunyan for logging

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var log = require('bunyan').createLogger({
+  name: 'baseapp',
+  stream: process.stdout,
+  level: 'info'
+});
+
 var app = express();
+
+app.use(require('express-bunyan-logger')({
+  name: 'logger',
+  streams: [{
+    level: 'info',
+    stream: process.stdout
+  }]
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,7 +29,6 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
