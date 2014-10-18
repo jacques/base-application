c30be27987aedd239fdea7464f6a374faab9fef4
Jacques Marneweck <jacques@powertrip.co.za>
2014-10-18 10:49:46 +0000

    More routes to show and edit the users profile page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,21 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Base Application' });
 });
 
+/* GET profile page */
+router.get('/profile', function(req, res) {
+  res.render('profile/index', { title: 'Base Application - Your Profile' });
+});
+
+/* POST profile page */
+router.post('/profile', function(req, res) {
+  res.render('profile/index', { title: 'Base Application - Your Profile' });
+});
+
+/* GET profile edit page */
+router.get('/profile/edit', function(req, res) {
+  res.render('profile/edit', { title: 'Base Application - Edit Your Profile' });
+});
+
 /* GET admin page */
 router.get('/admin', function(req, res) {
   res.render('admin/index', { title: 'Base Application - Admin' });
