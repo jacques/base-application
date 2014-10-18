52c0d1b6ee7ff8df2df8b5d2cf807ed725c901ed
Jacques Marneweck <jacques@powertrip.co.za>
2014-10-18 10:12:28 +0000

    Rough quick admin pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Base Application' });
 });
 
+/* GET admin page */
+router.get('/admin', function(req, res) {
+  res.render('admin/index', { title: 'Base Application - Admin' });
+});
+
+/* GET admin users page */
+router.get('/admin/users', function(req, res) {
+  res.render('admin/users/index', { title: 'Base Application - Admin - Users' });
+});
+
 module.exports = router;
